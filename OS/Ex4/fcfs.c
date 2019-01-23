b3091e37980b69fdc2cecd2d0fdb8d3a407a3002
Barathan Ganesh <demongod250299@gmail.com>
2019-01-23 13:17:11 +0530

    added arrival time and Gantt chart
    SJF in progress

--- OS/Ex4/fcfs.c
+++ OS/Ex4/fcfs.c
@@ -1,24 +1,109 @@
 #include<stdio.h>
-int main()
+#include<string.h>
+#include<stdlib.h>
+int n,Bu[20],Twt,Ttt,A[10],Wt[10],w;
+float Awt,Att;
+char pname[20][20],c[20][20];
+
+
+void Getdata()
 {
-    int i,n,process[20],wait[20],tat[20],burst[20];
-    printf("Enter the number of processes(<20) : ");
+    int i;
+    printf("\n Enter the number of processes: ");
     scanf("%d",&n);
-    printf("\nEnter the burst time of %d process continuously...\n",n);
-    wait[0]=0;
-    for(i = 0 ; i < n ; i++)
+    for(i=1;i<=n;i++)
     {
-        scanf("%d",&burst[i]);
-        if(i>0)
-            wait[i]=wait[i-1]+burst[i-1];
-        tat[i]=wait[i]+burst[i];
+        fflush(stdin);
+        printf("\n\n Enter the process name: ");
+        scanf("%s",pname[i]);
+        printf("\n Enter The BurstTime for Process %s = ",pname[i]);
+        scanf("%d",&Bu[i]);
+        printf("\n Enter the Arrival Time for Process %s =  ",pname[i]);
+        scanf("%d",&A[i]);
     }
-    for(i = 0 ; i < n ; i++)
-    {
-        printf("\nprocess : %d :\n",i+1);
-        printf("wait time : %d\n",wait[i]);
-        printf("burst time : %d\n",burst[i]);
-        printf("total turnaround time : %d\n",tat[i]);
-    }
-    return 0;
 }
+//DISPLAYING THE GANTT CHART
+void Gantt_chart()
+{
+    int i;
+    printf("\n\n\t\t\tGANTT CHART\n");
+    printf("\n-----------------------------------------------------------\n");
+    for(i=1;i<=n;i++)
+        printf("|\t%s\t",pname[i]);
+    printf("|\t\n");
+    printf("\n-----------------------------------------------------------\n");
+    printf("\n");
+    for(i=1;i<=n;i++)
+        printf("%d\t\t",Wt[i]);
+    printf("%d",Wt[n]+Bu[n]);
+    printf("\n-----------------------------------------------------------\n");
+    printf("\n");
+ }
+//CALCULATING AVERAGE WAITING TIME AND AVERAGE TURN AROUND TIME
+void Calculate()
+{
+    int i;
+    Wt[1]=0;
+    for(i=2;i<=n;i++)
+    {
+        Wt[i]=Bu[i-1]+Wt[i-1];
+    }
+    for(i=1;i<=n;i++)
+    {
+        Twt=Twt+(Wt[i]-A[i]);
+        Ttt=Ttt+((Wt[i]+Bu[i])-A[i]);
+    }
+    Att=(float)Ttt/n;
+    Awt=(float)Twt/n;
+    printf("\n\n Average Turn around time=%3.2f ms ",Att);
+    printf("\n\n AverageWaiting Time=%3.2f ms",Awt);
+}
+//FCFS Algorithm
+void fcfs()
+{
+    int i,j,temp, temp1;
+    Twt=0;
+    Ttt=0;
+    printf("\n\n FIRST COME FIRST SERVED ALGORITHM\n\n");
+    for(i=1;i<=n;i++)
+    {
+        for(j=i+1;j<=n;j++)
+        {
+            if(A[i]>A[j])
+            {
+                temp=Bu[i];
+                temp1=A[i];
+                Bu[i]=Bu[j];
+                A[i]=A[j];
+                Bu[j]=temp;
+                A[j]=temp1;
+                strcpy(c[i],pname[i]);
+                strcpy(pname[i],pname[j]);
+                strcpy(pname[j],c[i]);
+            }
+        }
+    }
+    Calculate();
+    Gantt_chart();
+}
+
+
+void main()
+{
+    int ch;
+    Getdata();
+    printf("\n Enter your choice:\n1.FCFS\n2.SJF\n3.EXIT");
+    scanf("%d",&ch);
+    switch(ch)
+    {
+	case 1:
+    		fcfs();
+    		break;
+        case 2:
+		printf("in progress");
+		break;
+	case 3:
+		exit(1);
+		break;
+    }
+}	
