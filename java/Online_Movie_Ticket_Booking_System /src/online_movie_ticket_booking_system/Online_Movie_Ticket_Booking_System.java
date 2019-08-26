98e3f3cd0fc38903afc46304208d4ad75a2a788f
brthngnsh <demongod250299@gmail.com>
2019-08-26 19:12:31 +0530

    added UI for java OOAD project

---
+++ java/Online_Movie_Ticket_Booking_System /src/online_movie_ticket_booking_system/Online_Movie_Ticket_Booking_System.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package online_movie_ticket_booking_system;
+
+/**
+ *
+ * @author taskmaster2502
+ */
+public class Online_Movie_Ticket_Booking_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
